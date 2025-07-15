5e0635082781edf15992ff49a2b506970a7aca0b
김해빈 <113007354+Kimheabin@users.noreply.github.com>
2025-07-16 01:27:06 +0900

    수정1

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,10 @@ import ButtonTest from "./components/Button/ButtonTest";
 import InputTest from "./components/Input/InputTest";
 import "./style/global.css";
 import "./style/variables.css";
+import "./index.css";
 
 function App() {
-  return (
-    <div>
-      <h1 style={{ padding: "50px 50px 0px 50px" }}>공통 컴포넌트 테스트</h1>
-      <div style={{ width: "440px" }}>
-        <InputTest />
-        <ButtonTest />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
