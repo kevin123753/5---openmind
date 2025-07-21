4584b9e27262d3d8b289b362c19e9c041c582824
김해빈 <113007354+Kimheabin@users.noreply.github.com>
2025-07-21 15:46:43 +0900

    kakaotest

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
+const kakaoKey = import.meta.env.VITE_KAKAO_JS_KEY;
+if (window.Kakao && !window.Kakao.isInitialized()) {
+  window.Kakao.init(kakaoKey);
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
